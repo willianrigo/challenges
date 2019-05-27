69a9ea7dac4d6ef28f7fab0fc551efa71e820194
Willian Rigo <willianrigo@hero99.com.br>
2019-05-27 03:28:16 -0300

    feat: CardButton component'

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import CardButton from './src/components/Card/CardButton'
 import IconLabel from './src/components/Card/IconLabel'
-
+ 
 export default class App extends React.Component {
   
   render() {
 
     return (
       <View style={styles.container}>
-        <IconLabel name={"home"} type={"FontAwesome"}size={"Medium"} color={"Primary"}>Teste</IconLabel>
+        <CardButton type={'FAB'}>teste</CardButton>
+        {/* <IconLabel size="Large" name="Home" type="FontAwesome"></IconLabel> */}
       </View>
     );
   }
---
+++ src/components/Card/CardButton.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { Button, View, Icon } from 'native-base'
+import PropTypes from "prop-types";
+import * as constants from '../../utils/constants'
+
+export default CardButton = (props) => {
+    
+    const {
+        type,
+    } = props
+
+        if(props.type === 'Primary')
+            return(
+                <View style={{
+                    width: 200,
+                    height: 60,
+                }}>
+                    <Button primary 
+                        style={{
+                            width: "100%",
+                            height: "100%",
+                            backgroundColor: '#8c388c',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            ...props.style
+                        }}>
+                        <Text style={{fontWeight: 'bold', textAlign: 'center', color: 'white'}}>
+                            {props.children}
+                        </Text>
+                    </Button>
+                </View>
+            )
+
+        else if(props.type === 'FAB'){
+            return(
+                <View>
+                    <Button 
+                        
+                        style={{
+                            backgroundColor: '#FFFFFF',
+                            borderColor: 'rgba(0, 0, 0, 0.1)',
+                            borderRadius: 50,
+                            ...props.style
+                            }}>
+                            
+                        <Icon 
+                            name='play'
+                            style={{
+                                color: '#943E95',
+                            }}
+                        /> 
+                    </Button>
+                </View>
+            )
+        }
+    }
+
+CardButton.propTypes = {
+    style: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.number,
+      PropTypes.array
+    ]),
+    size: PropTypes.string,
+    color: PropTypes.string,
+    display: PropTypes.string,
+    type: PropTypes.string
+}
